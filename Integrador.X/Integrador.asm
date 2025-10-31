d869da6a35eb8a9f2172805067e083d4fbea7604
Marcelo Tapia <marcetapia060@mi.unc.edu.ar>
2025-10-31 20:50:57 -0300

    Configuración de bits

---
+++ Integrador.X/Integrador.asm
@@ -0,0 +1,17 @@
+; PIC16F887 Configuration Bit Settings
+
+; Assembly source line config statements
+
+#include "p16f887.inc"
+
+; CONFIG1
+; __config 0x23E1
+ __CONFIG _CONFIG1, _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0x3FFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+
+
+
+
+
